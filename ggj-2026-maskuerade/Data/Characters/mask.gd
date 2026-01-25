7a14a80cc988e181833c1177e039527dc1efd7a3
Nathan Solberg <natedsolberg@gmail.com>
2026-01-25 18:50:10 -0500

    Mask Swapping
    - Added option to mask swap with E and Q, iterating through the frames to show the right mask
    - Define Frame Max in Mask scene

--- ggj-2026-maskuerade/Data/Characters/mask.gd
+++ ggj-2026-maskuerade/Data/Characters/mask.gd
@@ -1,14 +1,17 @@
 @tool
 extends Node2D
 
+class_name Mask
+
 @export var mask_index : int = 0
+@export var max_mask_index : int = 0
 @export var projectile : PackedScene
 
 @onready var mask_sprite: Sprite2D = %MaskSprite
 @onready var shoot_point: Node2D = %ShootPoint
 
 func _ready() -> void:
-	mask_sprite.frame = mask_index
+	_UpdateSwitchedMask()
 
 func _process(_delta: float) -> void:
 	shoot_point.look_at(get_global_mouse_position())
@@ -25,3 +28,15 @@ func _unhandled_input(event: InputEvent) -> void:
 		
 		inst.global_position = shoot_point.global_position
 		inst.global_rotation = shoot_point.global_rotation
+
+func OnSwitchNextMask():
+	mask_index = wrapi(mask_index + 1, 0, max_mask_index - 1)
+	_UpdateSwitchedMask()
+
+func OnSwitchPrevMask():
+	mask_sprite.frame
+	mask_index = wrapi(mask_index - 1, 0, max_mask_index - 1)
+	_UpdateSwitchedMask()
+
+func _UpdateSwitchedMask():
+	mask_sprite.frame = mask_index
--- ggj-2026-maskuerade/Data/Characters/player.gd
+++ ggj-2026-maskuerade/Data/Characters/player.gd
@@ -4,16 +4,18 @@ extends CharacterBody2D
 const SPEED = 175.0
 const JUMP_VELOCITY = -400.0
 
+@onready var maskRef : Mask = $Mask
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("DebugMaskNext"):
+		maskRef.OnSwitchNextMask()
+		get_viewport().set_input_as_handled()
+	if event.is_action_pressed("DebugMaskPrev"):
+		maskRef.OnSwitchPrevMask()
+		get_viewport().set_input_as_handled()
 
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
 
+func _physics_process(_delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_vector("MoveLeft", "MoveRight", "MoveUp", "MoveDown")
