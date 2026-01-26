f9834e241e49d43224a351278bbda3fb9d42ea9a
Kolby <kolbyheiss@gmail.com>
2026-01-25 20:40:26 -0800

    Added Enemy with simple state machine and basic hp bar

--- ggj-2026-maskuerade/Data/Characters/projectile.gd
+++ ggj-2026-maskuerade/Data/Characters/projectile.gd
@@ -3,6 +3,7 @@ class_name Projectile
 
 @export var speed := 5.0
 @export var dir: Vector2 = Vector2(-1,0)
+@export var damage: = 2.0
 
 #func _physics_process(_delta: float) -> void:
 	#position += dir * speed 
