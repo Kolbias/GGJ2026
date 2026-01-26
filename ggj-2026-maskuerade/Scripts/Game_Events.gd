0fe45908680164be96a09ec82915251d791652b1
Kolby <kolbyheiss@gmail.com>
2026-01-25 21:40:57 -0800

    Altered Game_Events to fix merge conflict?

--- ggj-2026-maskuerade/Scripts/Game_Events.gd
+++ ggj-2026-maskuerade/Scripts/Game_Events.gd
@@ -1,9 +1,7 @@
 extends Node
 
-<<<<<<< HEAD
-signal shot_fired(shoot_position: Vector2, mouse_pos: Vector2)
+#signal shot_fired(shoot_position: Vector2, mouse_pos: Vector2)
 
 signal room_exited
-=======
+
 signal shot_fired(shoot_position: Vector2, mouse_pos: Vector2, mask_index: String)
->>>>>>> 3456add8f7b228bbed0bc6e398b3e90c93dcc490
