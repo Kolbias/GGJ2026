5511111d8e8479b338b047fe311f51c22d974509
Nathan Solberg <natedsolberg@gmail.com>
2026-01-28 13:30:37 -0500

    Added Fire Rate for bullets
    - Added Fire Rate (shots per second) in the mask resource and set fox to 3 and the others to 1
    - Now you can hold down the fire button instead of spam clicking

--- ggj-2026-maskuerade/Data/Characters/mask.gd
+++ ggj-2026-maskuerade/Data/Characters/mask.gd
@@ -5,6 +5,8 @@ class_name Mask
 
 var currentMask : MaskResource = preload("res://Data/Resources/Masks/EmptyMask.tres")
 
+var currentWeaponTimer : float = 0.0
+var canFireProjectile : bool = true
 @export var projectile : PackedScene
 
 @onready var mask_sprite: Sprite2D = %MaskSprite
@@ -13,12 +15,20 @@ var currentMask : MaskResource = preload("res://Data/Resources/Masks/EmptyMask.t
 func _ready() -> void:
 	pass
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	shoot_point.look_at(get_global_mouse_position())
 	
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("Shoot"):
-		GameEvents.shot_fired.emit(shoot_point.global_position, get_global_mouse_position(), currentMask.maskType)
+	if currentWeaponTimer > 0.0:
+		currentWeaponTimer -= delta
+		if currentWeaponTimer <= 0.0:
+			canFireProjectile = true
+		
+	if canFireProjectile == true:
+		if Input.is_action_pressed("Shoot"):
+			currentWeaponTimer = (1.0 / currentMask.projectileFireRate)
+			canFireProjectile = false
+			GameEvents.shot_fired.emit(shoot_point.global_position, get_global_mouse_position(), currentMask.projectileType)
+
 
 func SetMask(maskRes : MaskResource):
 	currentMask = maskRes
